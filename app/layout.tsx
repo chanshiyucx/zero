2fadd7b42176acaecd74087be359f0344bc9a9e9
chanshiyu <me@chanshiyu.com>
2023-06-28 23:19:00 +0800

    feat: add site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,14 @@ export const metadata: Metadata = {
   description: '蝉鸣如雨 花宵道中',
   authors: [{ name: '蝉時雨', url: 'https://chanshiyu.com' }],
   keywords: 'Code, ACG, Zero, 蝉時雨',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
