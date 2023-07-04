015d5c28b2f03eabc8c4c00265fc0a8dcf28e9a5
chanshiyu <me@chanshiyu.com>
2023-07-04 23:03:27 +0800

    feat: add butterfly

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import MDX from '@/components/MDX'
 export default function Home() {
   const posts: Post[] = allPosts.sort((a, b) => b.no - a.no)
 
-  const post: Post = posts[0]
-  const list = posts.slice(0, 3)
-  console.log('list-----', list)
+  // const post: Post = posts[0]
+  // const list = posts.slice(0, 3)
+  // console.log('list-----', list)
   return (
     <div className="mx-auto max-w-xl py-8 ">
       <h1 className="mb-8 text-center text-2xl font-black">Home Page</h1>
---
+++ components/Icons/Butterfly/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import './index.css'
+
+const Butterfly: FC = () => {
+  return (
+    <div className="z-10 mx-3 flex cursor-pointer items-center justify-center">
+      <div className="butterfly"></div>
+    </div>
+  )
+}
+
+export default Butterfly
