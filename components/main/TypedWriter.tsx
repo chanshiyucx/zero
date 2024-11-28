7aea4cf5557fa6efcfedbb42a40e335c8d41d30f
xin <chanshiyucx@gmail.com>
2024-11-28 15:30:45 +0100

    refactor: main title

---
+++ components/main/TypedWriter.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+import Typed from 'typed.js'
+
+const sentences: string[] = [
+  'Tech stuff enthusiast',
+  'Nature admirer',
+  'Enjoyer of good books',
+  'Full stack developer',
+]
+
+export function TypedWriter() {
+  const TypedElement = useRef(null)
+
+  useEffect(() => {
+    if (!TypedElement.current) return
+
+    const typed = new Typed(TypedElement.current, {
+      strings: sentences,
+      typeSpeed: 80,
+      backSpeed: 50,
+      startDelay: 0,
+      backDelay: 2000,
+      shuffle: false,
+      loop: true,
+    })
+
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+
+  return <span ref={TypedElement}></span>
+}
---
+++ hook/index.ts
@@ -0,0 +1,3 @@
+export * from './use-is-mounted'
+export * from './use-loading'
+export * from './use-local-storage'
