286aca3a5f8bcdd185887f8d5404fbe8faf101da
chanshiyucx <chanshiyucx@gmail.com>
2024-12-02 15:50:57 +0100

    style: change theme opacity

--- components/ui/mdx/CopyButton.tsx
+++ components/ui/mdx/CopyButton.tsx
@@ -23,10 +23,8 @@ export const CopyButton = ({ text }: CopyButtonProps) => {
     <button
       disabled={isCopied}
       onClick={copy}
-      className={`absolute bottom-0 right-0 flex items-center gap-1 rounded-tl-lg p-2 leading-none hover:opacity-100 active:opacity-100 ${
-        isCopied
-          ? 'bg-green-100/40 text-green-800 opacity-100 dark:bg-green-300/10 dark:text-green-500'
-          : 'cursor-pointer bg-neutral-300 text-neutral-600 opacity-40 dark:bg-neutral-800 dark:text-neutral-400'
+      className={`absolute bottom-0 right-0 flex items-center gap-1 rounded-tl-lg p-2 leading-none hover:bg-overlay active:bg-overlay ${
+        isCopied ? 'bg-overlay' : 'cursor-pointer bg-base text-subtle'
       }`}
     >
       {isCopied ? <Check size="1em" /> : <Copy size="1em" />}
