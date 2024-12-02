6ca28eb5a35f04e491887a15533ff3776c044b81
chanshiyucx <chanshiyucx@gmail.com>
2024-12-02 17:23:05 +0100

    style: add copy button animation

--- components/ui/mdx/CopyButton.tsx
+++ components/ui/mdx/CopyButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Check, Copy } from '@phosphor-icons/react/dist/ssr'
+import clsx from 'clsx'
 import { useState } from 'react'
 
 interface CopyButtonProps {
@@ -23,9 +24,10 @@ export const CopyButton = ({ text }: CopyButtonProps) => {
     <button
       disabled={isCopied}
       onClick={copy}
-      className={`absolute bottom-0 right-0 flex items-center gap-1 rounded-tl-lg p-2 leading-none hover:bg-overlay active:bg-overlay ${
-        isCopied ? 'bg-overlay' : 'cursor-pointer bg-base text-subtle'
-      }`}
+      className={clsx(
+        'absolute right-0 top-0 flex items-center rounded-bl-lg p-2 leading-none opacity-0 transition-all duration-300 group-hover:opacity-100 active:opacity-100',
+        isCopied ? 'bg-overlay' : 'cursor-pointer bg-base',
+      )}
     >
       {isCopied ? <Check size="1em" /> : <Copy size="1em" />}
     </button>
