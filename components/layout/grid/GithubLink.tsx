db9b83ed12ff9a514d2eafdf39a699e835450ade
xin <chanshiyucx@gmail.com>
2024-12-18 17:38:42 +0100

    feat: update github stats svg

--- components/layout/grid/GithubLink.tsx
+++ components/layout/grid/GithubLink.tsx
@@ -1,28 +1,50 @@
-import { GithubLogo } from '@phosphor-icons/react/dist/ssr'
 import Image from 'next/image'
+import { getGithubRepositories, getGithubUserData } from '@/lib/api'
 import { config } from '@/lib/constants/config'
 
-export function GithubLink() {
+interface GitHubStatsDataProps {
+  label: string
+  value: number
+}
+
+function GitHubStatsData({ label, value }: GitHubStatsDataProps) {
+  return (
+    <div>
+      <span className="mr-1 text-sm">{label}:</span>
+      {value}
+    </div>
+  )
+}
+
+export async function GithubLink() {
+  const { followers, public_repos } = await getGithubUserData()
+  const repositories = await getGithubRepositories()
+  const stars = repositories.reduce(
+    (acc, repo) => acc + repo.stargazers_count,
+    0,
+  )
+
   return (
     <a
       href={config.links.github}
       target="_blank"
       rel="noopener noreferrer"
-      className="card relative block h-full w-full"
+      className="card relative block h-36 w-full bg-surface text-base"
     >
       <Image
         src="/assets/cat.jpg"
         alt="Cat"
         fill
         sizes="(max-width: 768px) 100vw, 50vw"
-        className="-z-10 brightness-50"
+        className="-z-10 object-cover brightness-50"
       />
-      <div className="flex h-full flex-col justify-between gap-5 p-6 text-base font-bold">
-        <GithubLogo className="text-xl" weight="bold" />
-        <span className="flex flex-col space-y-0.5">
-          <span>GitHub</span>
-          <span className="text-sm">My experiments (aka projects)</span>
-        </span>
+      <div className="h-full p-3 font-bold">
+        <h2 className="p-1 text-xl">@chanshiyucx</h2>
+        <div className="absolute bottom-3 right-5 flex flex-col flex-wrap gap-2 self-end">
+          <GitHubStatsData label="Stars" value={stars} />
+          <GitHubStatsData label="Followers" value={followers} />
+          <GitHubStatsData label="Repos" value={public_repos} />
+        </div>
       </div>
     </a>
   )
