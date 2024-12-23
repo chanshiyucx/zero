e5512501a5c0a8b897fc6fec5b9f2001c40af877
chanshiyucx <chanshiyucx@gmail.com>
2024-12-23 10:16:22 +0100

    style: add run cat

--- components/layout/main/Title.tsx
+++ components/layout/main/Title.tsx
@@ -1,7 +1,10 @@
+import { random } from '@/lib/utils/helper'
 import { Explore } from './Explore'
 import { TypedWriter } from './TypedWriter'
 
 export function Title() {
+  const cat = random(0, 1) === 0 ? 'walk-cat' : 'run-cat'
+
   return (
     <div className="flex h-fit w-full flex-col gap-20">
       <div className="relative flex items-center justify-between max-md:justify-center">
@@ -12,7 +15,7 @@ export function Title() {
           </div>
         </div>
         <div className="relative h-36 w-72 max-md:absolute max-md:left-1/2 max-md:top-1/2 max-md:-translate-x-1/2 max-md:-translate-y-1/2 max-md:opacity-10">
-          <div className="walk-cat"></div>
+          <div className={cat}></div>
         </div>
       </div>
       <Explore />
