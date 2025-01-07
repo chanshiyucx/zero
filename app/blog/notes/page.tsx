536cc45bacb49c7cbe6bb01e1c02b6c7abfb65f6
chanshiyucx <chanshiyucx@gmail.com>
2025-01-07 14:11:27 +0100

    style: modified notes layout style

--- app/blog/notes/page.tsx
+++ app/blog/notes/page.tsx
@@ -1,6 +1,6 @@
 import type { Note } from 'content-collections'
 import type { Metadata } from 'next'
-import { Tag } from '@phosphor-icons/react/dist/ssr'
+import { CalendarBlank, Tag } from '@phosphor-icons/react/dist/ssr'
 import { Date } from '@/components/ui/date'
 import { Divider } from '@/components/ui/divider'
 import { MDX } from '@/components/ui/mdx'
@@ -18,26 +18,25 @@ export const metadata: Metadata = {
 
 function NoteItem({ note }: NoteItemProps) {
   return (
-    <div className="flex flex-row pb-12 max-md:flex-col max-md:gap-5">
-      <div className="sticky top-8 h-fit flex-1 max-md:static">
-        <h2 className="font-bold">{note.title}</h2>
-        <div className="flex flex-col gap-5 max-md:flex-row max-md:justify-between">
-          <Date
-            dateString={note.date}
-            dateFormat="LLL dd, yyyy"
-            className="text-sm"
-          />
-          <div className="flex flex-col gap-1 text-sm max-md:flex-row max-md:gap-3">
+    <div>
+      <div className="sticky top-0 z-10 flex flex-1 flex-row items-center justify-between bg-base py-3">
+        <h2 className="text-2xl font-bold">{note.title}</h2>
+        <div className="flex gap-5 text-subtle">
+          <span className="inline-flex items-center gap-1">
+            <CalendarBlank weight="bold" />
+            <Date dateString={note.date} dateFormat="LLL dd, yyyy" />
+          </span>
+          <span className="flex gap-3">
             {note.tags.map((tag) => (
               <span key={tag} className="inline-flex items-center gap-1">
-                <Tag />
+                <Tag weight="bold" />
                 {tag}
               </span>
             ))}
-          </div>
+          </span>
         </div>
       </div>
-      <div className="w-3/4 max-md:w-full">
+      <div className="w-full pb-12 pt-2">
         <MDX code={note.contentCode} />
       </div>
     </div>
@@ -52,7 +51,7 @@ export default function Page() {
       <header>
         <h1 className="text-4xl font-extrabold">Notes are memory anchors.</h1>
       </header>
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-8">
         {noteList.map((note, index) => (
           <div key={note.title}>
             <NoteItem note={note} />
