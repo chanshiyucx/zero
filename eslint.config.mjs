83a6decdaca984b6216abc4905132d5962056af2
xin <chanshiyucx@gmail.com>
2025-06-22 12:50:50 +0200

    chore: modified eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,48 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+import js from '@eslint/js'
+import nextPlugin from '@next/eslint-plugin-next'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
 
 /** @type {import('eslint').Linter.Config[]} */
-const configs = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+export default [
+  {
+    ignores: ['.next/**', 'out/**', 'node_modules/**', 'dist/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.es2021,
+        ...globals.browser,
+        ...globals.node,
+        React: 'readonly',
+        JSX: 'readonly',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': typescript,
+      '@next/next': nextPlugin,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
 ]
-
-export default configs
