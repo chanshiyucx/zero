b5e1a5c01e05feb3d83123448651ef9934e1a7df
xin <chanshiyucx@gmail.com>
2025-07-24 17:43:26 +0200

    fix: mdx components missing audio

---
+++ components/ui/mdx/audio.tsx
@@ -0,0 +1,11 @@
+import type { AudioHTMLAttributes, DetailedHTMLProps } from 'react'
+import { AudioPlayer } from '@/components/ui/audio-player'
+
+export function Audio(
+  props: DetailedHTMLProps<
+    AudioHTMLAttributes<HTMLAudioElement>,
+    HTMLAudioElement
+  >,
+) {
+  return <AudioPlayer src={props.src as string} />
+}
