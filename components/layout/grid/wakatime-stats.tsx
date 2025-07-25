26bc9a91800b15b3cb48a03ae3f9ee35b94052df
xin <chanshiyucx@gmail.com>
2025-07-25 22:53:31 +0200

    style: add cursor spin animation

--- components/layout/grid/wakatime-stats.tsx
+++ components/layout/grid/wakatime-stats.tsx
@@ -14,12 +14,9 @@ export async function WakatimeStats() {
       rel="noopener noreferrer"
       className="card-sm bg-rose dark:bg-rose/75 relative flex h-[4.125rem] flex-col items-center justify-center text-base"
     >
-      <Cursor className="absolute top-2 left-3 -rotate-45 text-5xl opacity-80" />
+      <Cursor className="absolute top-2 left-3 -rotate-45 animate-spin text-5xl opacity-80 duration-10000" />
       <span className="inline-flex -rotate-2 items-center gap-1 font-mono text-3xl font-semibold">
-        <CodeIcon
-          className="animate-blink mr-2 inline-block text-xl"
-          weight="bold"
-        />
+        <CodeIcon className="mr-2 inline-block text-xl" weight="bold" />
         {Math.round(total_seconds / 3600)}h
       </span>
       <span className="text-sm">(coding stats)</span>
