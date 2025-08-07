e7ef0f7c40d698f8a829dd762ddea6e9ddc7605b
chanshiyucx <chanshiyucx@gmail.com>
2025-08-07 20:47:48 +0200

    feat: add discord link

--- components/layout/grid/discord-status.tsx
+++ components/layout/grid/discord-status.tsx
@@ -1,12 +1,18 @@
 import { DiscordLogoIcon } from '@phosphor-icons/react/dist/ssr'
 import { getDiscordData } from '@/lib/api'
+import { siteConfig } from '@/lib/constants/config'
 
 export async function DiscordStatus() {
   const { data } = await getDiscordData()
 
   return (
     <div className="flex h-36 w-fit items-center justify-center overflow-hidden rounded-lg p-2">
-      <div className="relative flex -rotate-12 items-center justify-center">
+      <a
+        href={siteConfig.links.discord}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative flex -rotate-12 items-center justify-center"
+      >
         <DiscordLogoIcon
           size="1em"
           className="text-overlay absolute -bottom-8 -z-10 h-32 w-32 -rotate-45 brightness-95 dark:brightness-110"
@@ -16,7 +22,7 @@ export async function DiscordStatus() {
           <span>{data.discord_status}</span>
           <span className="text-sm">(@chanshiyucx)</span>
         </p>
-      </div>
+      </a>
     </div>
   )
 }
