18c3a4256d39e4ce47e0db40c316d79281930346
chanshiyucx <chanshiyucx@gmail.com>
2025-08-08 11:26:30 +0200

    style: remove footer width auto

--- components/layout/grid/github-stats.tsx
+++ components/layout/grid/github-stats.tsx
@@ -19,7 +19,7 @@ export function GithubStats() {
           alt="Github Stats"
           width={362}
           height={144}
-          className="w-auto object-cover"
+          className="object-cover"
         />
       </a>
     </Card>
