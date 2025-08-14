afabae8933c8acd1ecd062ee6ede4114f1efb9d0
xin <chanshiyucx@gmail.com>
2025-08-14 22:55:31 +0200

    fix: useDeviceListener exec

--- app/providers/motion-provider.tsx
+++ app/providers/motion-provider.tsx
@@ -2,8 +2,11 @@
 
 import { domAnimation, LazyMotion } from 'framer-motion'
 import { type ReactNode } from 'react'
+import { useDeviceListener } from '@/hooks/use-device-listener'
 
 export default function MotionProvider({ children }: { children: ReactNode }) {
+  useDeviceListener()
+
   return (
     <LazyMotion features={domAnimation} strict>
       {children}
