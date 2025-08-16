5dd50e55ae092759828cf5acd20fac4a286ae98c
xin <chanshiyucx@gmail.com>
2025-08-16 21:11:52 +0200

    perf: stagger children for item

--- components/layout/main/explore.tsx
+++ components/layout/main/explore.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { ArrowRightIcon, CommandIcon } from '@phosphor-icons/react/dist/ssr'
+import { StaggeredFadeInItem } from '@/components/ui/stagger'
 import { useCommand } from '@/stores/use-command'
 
 export function Explore() {
   const { toggle } = useCommand()
 
   return (
-    <button
+    <StaggeredFadeInItem
+      as="button"
       onClick={toggle}
       aria-label="Open menu"
       className="group flex w-full items-center justify-center gap-1 text-sm tracking-widest opacity-60 hover:opacity-100 active:opacity-100"
@@ -23,6 +25,6 @@ export function Explore() {
         weight="bold"
         className="-translate-x-full opacity-0 duration-300 group-hover:translate-x-0 group-hover:opacity-100 max-md:translate-x-0 max-md:opacity-100"
       />
-    </button>
+    </StaggeredFadeInItem>
   )
 }
