e6117666edeee7db1989c730fb146b910db1c8c8
xin <chanshiyucx@gmail.com>
2025-08-18 22:00:49 +0200

    vercel: add @vercel/speed-insights

---
+++ app/providers/vercel-provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { type ReactNode } from 'react'
+
+export default function VercelProvider({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {children}
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
+}
