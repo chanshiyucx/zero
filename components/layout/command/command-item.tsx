327d2c94db6e178b1af42a91bede7e60fed1cdb5
chanshiyucx <chanshiyucx@gmail.com>
2025-08-23 18:43:17 +0200

    perf: command onSelect

--- components/layout/command/command-item.tsx
+++ components/layout/command/command-item.tsx
@@ -1,13 +1,11 @@
 import { Command } from 'cmdk'
-import { useCallback, type ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import { cn } from '@/lib/utils/style'
-import { useCommand } from '@/stores/use-command'
 
 interface CommandItemProps {
   onSelect?: () => void
   icon?: ReactNode
   shortcut?: string[]
-  page?: string
   className?: string
   children: ReactNode
 }
@@ -17,22 +15,12 @@ export const CommandItem = ({
   icon,
   shortcut,
   children,
-  page,
   className,
 }: CommandItemProps) => {
-  const { pushPage } = useCommand()
-  const handleSelect = useCallback(() => {
-    if (page) {
-      pushPage(page)
-    } else if (onSelect) {
-      onSelect()
-    }
-  }, [page, onSelect, pushPage])
-
   return (
     <Command.Item
-      onSelect={handleSelect}
-      className="text-subtle data-[selected=true]:bg-muted/10 data-[selected=true]:text-text m-0 flex cursor-pointer items-center justify-between gap-5 rounded-lg p-2 transition-colors"
+      onSelect={onSelect}
+      className="text-subtle m-0 flex cursor-pointer items-center justify-between gap-5 rounded-lg p-2 transition-colors"
     >
       <div className="flex shrink-0 items-center">
         {icon}
