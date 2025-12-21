9713d61a9258e33d56ed1bf4712027552185fdab
xin <xinchen1@sungrowpower.com>
2025-12-21 09:15:05 +0100

    feat: add use cache

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,12 @@
 import type { MetadataRoute } from 'next'
+import { cacheLife } from 'next/cache'
 import { siteConfig } from '@/lib/constants/config'
 import { sortedContent } from '@/lib/utils/content'
 
-export const dynamic = 'force-static'
-
 export default function sitemap(): MetadataRoute.Sitemap {
+  'use cache'
+  cacheLife('max')
+
   const contentRoutes = sortedContent.map((item) => ({
     url: `${siteConfig.host}${item.url}`,
     lastModified: new Date(item.date),
