04ac5fea9a8d171eb1bcedeeeca5adaf246a76e3
xin <chanshiyucx@gmail.com>
2025-12-21 11:35:38 +0100

    feat: remove sitemap cache

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,8 @@
 import type { MetadataRoute } from 'next'
-import { cacheLife } from 'next/cache'
 import { siteConfig } from '@/lib/constants/config'
 import { sortedContent } from '@/lib/utils/content'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  'use cache'
-  cacheLife('max')
-
   const contentRoutes = sortedContent.map((item) => ({
     url: `${siteConfig.host}${item.url}`,
     lastModified: new Date(item.date),
