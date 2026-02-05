fed47fa46a7a589286587a29c033a13e519b8f0f
xin <chanshiyucx@gmail.com>
2026-02-05 22:59:15 +0100

    fix: add generateStaticParams

--- app/journal/[slug]/page.tsx
+++ app/journal/[slug]/page.tsx
@@ -1,1 +1,9 @@
+import { sortedJournals } from '@/lib/utils/content'
+
+export function generateStaticParams() {
+  return sortedJournals.map((journal) => ({
+    slug: journal.slug,
+  }))
+}
+
 export { default, generateMetadata } from '@/components/article'
--- app/snippets/[slug]/page.tsx
+++ app/snippets/[slug]/page.tsx
@@ -1,1 +1,9 @@
+import { sortedSnippets } from '@/lib/utils/content'
+
+export function generateStaticParams() {
+  return sortedSnippets.map((snippet) => ({
+    slug: snippet.slug,
+  }))
+}
+
 export { default, generateMetadata } from '@/components/article'
