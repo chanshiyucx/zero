863358cb782d460f422edc7e061f53bda8d75fcc
chanshiyucx <chanshiyucx@gmail.com>
2026-02-05 22:59:15 +0100

    fix: add generateStaticParams

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,1 +1,9 @@
+import { sortedArticles } from '@/lib/utils/content'
+
+export function generateStaticParams() {
+  return sortedArticles.map((article) => ({
+    slug: article.slug,
+  }))
+}
+
 export { default, generateMetadata } from '@/components/article'
