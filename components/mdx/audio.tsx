5862e403b625cb383b0e87bab54b3fda27579dbd
chanshiyucx <chanshiyucx@gmail.com>
2026-03-01 10:49:23 +0100

    feat: remove audio component

--- components/mdx/audio.tsx
+++
@@ -1,7 +0,0 @@
-import type { ComponentPropsWithoutRef } from 'react'
-import { AudioPlayer } from '@/components/audio-player'
-
-export function Audio(props: ComponentPropsWithoutRef<'audio'>) {
-  if (!props.src) return null
-  return <AudioPlayer src={props.src as string} />
-}
