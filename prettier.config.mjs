43501a4e0d65719b4b4eb4beae33bcfc05384b1b
xin <xin.chen@neuverge-tron.com>
2026-03-13 13:41:40 +0100

    chore: remove babel-plugin-react-compiler

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -25,7 +25,7 @@ const config = {
     'classProperties',
     'decorators-legacy',
   ],
-  importOrderTypeScriptVersion: '5.0.0',
+  importOrderTypeScriptVersion: '5.9.0',
   tailwindFunctions: ['clsx', 'cn'],
 }
 
