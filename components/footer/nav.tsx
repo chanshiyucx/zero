23c18904354da53f1d7227ad22d2aec17eb43f3c
chanshiyucx <chanshiyucx@gmail.com>
2026-03-17 17:38:04 +0100

    feat: footer internal link jump

--- components/footer/nav.tsx
+++ components/footer/nav.tsx
@@ -9,7 +9,7 @@ import { siteConfig } from '@/lib/constants/config'
 import { ThemeSwitcher } from './theme-switcher'
 
 const socialLinks = [
-  { href: '/blogroll', icon: ScrollIcon, label: 'Blogroll' },
+  { href: '/blogroll', icon: ScrollIcon, label: 'Blogroll', internal: true },
   { href: '/feed', icon: RssSimpleIcon, label: 'RSS Feed' },
   // { href: '/sitemap', icon: CompassIcon, label: 'Sitemap',  },
   {
@@ -22,22 +22,20 @@ const socialLinks = [
     icon: GithubLogoIcon,
     label: 'GitHub',
   },
-] as const
+]
 
 export function Nav() {
   return (
     <div className="flex gap-3">
       <ThemeSwitcher />
 
-      {socialLinks.map(({ href, icon: Icon, label }) => {
+      {socialLinks.map(({ href, icon: Icon, label, internal = false }) => {
+        const externalProps = internal
+          ? {}
+          : ({ target: '_blank', rel: 'noopener noreferrer' } as const)
+
         return (
-          <a
-            key={label}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={href}
-            aria-label={label}
-          >
+          <a key={label} href={href} aria-label={label} {...externalProps}>
             <Icon
               weight="fill"
               className="hover:text-subtle text-muted transition-colors duration-300"
