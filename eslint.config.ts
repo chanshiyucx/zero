a1d87ec6f6a879eb7cb491b9417c1640a78c4812
chanshiyucx <chanshiyucx@gmail.com>
2026-04-11 09:40:51 +0200

    chore: remove baseUrl configuration from tsconfig.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,68 +1,18 @@
-import js from '@eslint/js'
-import nextPlugin from '@next/eslint-plugin-next'
-import reactPlugin from 'eslint-plugin-react'
-import reactCompilerPlugin from 'eslint-plugin-react-compiler'
-import reactHooksPlugin from 'eslint-plugin-react-hooks'
-import tseslint from 'typescript-eslint'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  {
-    ignores: [
-      '**/.next/**',
-      '**/out/**',
-      '**/dist/**',
-      '**/node_modules/**',
-      '**/.content-collections/**',
-      'next-env.d.ts',
-    ],
-    linterOptions: {
-      reportUnusedDisableDirectives: 'error',
-    },
-  },
-
-  {
-    ...js.configs.recommended,
-    files: ['**/*.{ts,tsx}'],
-  },
-  ...tseslint.configs.recommendedTypeChecked.map((conf) => ({
-    ...conf,
-    files: ['**/*.{ts,tsx}'],
-  })),
-
-  {
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      '@next/next': nextPlugin,
-      react: reactPlugin,
-      'react-compiler': reactCompilerPlugin,
-      'react-hooks': reactHooksPlugin,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      ...reactPlugin.configs.flat?.recommended?.rules,
-      ...reactPlugin.configs.flat?.['jsx-runtime']?.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      'react-compiler/react-compiler': 'error',
-    },
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    files: ['**/*.config.{js,ts,mjs}', 'eslint.config.ts'],
-    ...tseslint.configs.disableTypeChecked,
-  },
-]
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'dist/**',
+    '.content-collections/**',
+    'next-env.d.ts',
+  ]),
+])
